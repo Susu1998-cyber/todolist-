7c8fd3c7dcfb90b62b740235747af08b8be129d1
susanth <susanthsankar1998@gmail.com>
2024-01-17 22:18:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+ 
+import './App.css';
+import TodoList from './Components/TodoList';
+import './App.css'
+ 
+
+function App() {
+  return (
+    <div className="App">
+<h1 style={{ color: 'red' }}>My TO-DO App</h1>
+     <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/TodoItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const TodoItem = ({task,deleteTask,toggleCompleted}) => {
+    const handleChange = () =>{
+        toggleCompleted(task.id)
+    }
+
+
+  return (
+    <div className='todo=item'>
+        <input
+         type='checkbox'
+         checked = {task.completed}
+         onChange = {handleChange}
+        />
+       <p>{task.text}</p>
+       <button onClick={() => deleteTask(task.id)} >X </button>
+    </div>
+  )
+}
+
+export default TodoItem
